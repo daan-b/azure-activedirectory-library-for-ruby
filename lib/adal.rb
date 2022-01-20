e998669ba90e3dc8cee24b76c8e141a712c98469
daan-b <44999910+daan-b@users.noreply.github.com>
2022-01-20 12:17:49 +0100

    Update adal.rb

--- lib/adal.rb
+++ lib/adal.rb
@@ -20,5 +20,6 @@
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
 
+require_relative './adal/request_parameters.rb'
 # Extract all Ruby files in $DIR/lib/adal/ regardless of where the gem is built.
 Dir[File.expand_path('../adal/*.rb', __FILE__)].each { |f| require_relative f }
